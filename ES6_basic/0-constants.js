ab02a49aa2e1b4eae187f4597da94b273e405885
Juandicode <juandiegomhs@mail.com>
2025-03-28 12:15:48 -0300

    const y let

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -13,3 +13,16 @@ export function taskNext() {
 
   return combination;
 }
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  const combination = 'But sometimes let' + getLast();
+  return combination;
+}
