6d7cfc329f52f67bf915919d429112f799e212d6
juandicode <juandiegomhs@gmail.com>
2025-03-29 21:48:01 -0300

    w

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,11 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
-    if (task && !task2) {
-
-    }
+    return [task, task2];
   }
+
   return [task, task2];
 }
