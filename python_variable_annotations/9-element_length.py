f776442496951ecfb73e329b247d8d99abeab2c8
Juandicode <juandiegomhs@mail.com>
2025-07-30 15:44:49 -0300

    retornarr suma d ela lista

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from typing import Iterable, Sequence, List, Tuple
 """Module that defines a type-annotated function to add two floats."""
+
+from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
