b74d38f3b693535cbe58967884758159acaf5ee5
juandicode <juandiegomhs@gmail.com>
2025-08-17 21:51:40 -0300

    authpy

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,12 +3,12 @@
 Main file
 """
 
-index_range = __import__('0-simple_helper_function').index_range
+from typing import Tuple
 
-res = index_range(1, 7)
-print(type(res))
-print(res)
 
-res = index_range(page=3, page_size=15)
-print(type(res))
-print(res)
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """returns a tuple containing the start and end index for pagination and
+    calculate the start and end index."""
+    index1 = (page - 1) * page_size
+    index2 = page * page_size
+    return index1, index2
